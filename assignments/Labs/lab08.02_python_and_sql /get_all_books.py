76cd4f179a8763d343ef6fa4fad77cf51bf9b283
Kate Lisovenko <kate.spb@gmail.com>
2024-11-26 12:06:48 +0000

    lab SQL lite. get all books

--- assignments/Labs/lab08.02_python_and_sql /get_all_books.py
+++ assignments/Labs/lab08.02_python_and_sql /get_all_books.py
@@ -2,3 +2,15 @@ import sqlite3
 con = sqlite3.connect('pfda.db')
 cur = con.cursor()
 
+# to get all rows
+result = cur.execute('SELECT * FROM book2')
+print(result.fetchall())
+
+# another way to get all rows
+for row in cur.execute('SELECT * from book2'):
+    print(f'row: {row}')
+
+
+# to get UNIQUE values: 
+for row in cur.execute('SELECT DISTINCT ISBN, title, author FROM book2'):
+    print(f'Row: {row}')
